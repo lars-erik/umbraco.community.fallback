859afb95edc29927b1cba16f4fafda06383b80d9
Lars-Erik <lea@markedspartner.no>
2023-06-21 00:53:55 +0200

    Initial commit - can shadow other editors! 🥳

---
+++ readme.md
@@ -0,0 +1,11 @@
+## Umbraco Community Fallback
+
+And effort to combine Admin Only Property shadowing with Wholething Fallback.
+
+To be continued!
+
+Creds to
+
+* @deanleigh
+* @leekelleher
+* @lottepitcher
---
+++ src/Umbraco.Community.Fallback/wwwroot/fallback.js
@@ -0,0 +1,7 @@
+﻿angular.module('umbraco').controller('umbraco.community.fallback.controller', [
+    '$scope',
+    function (scope) {
+        scope.shadowModel = JSON.parse(JSON.stringify(scope.model));
+        scope.shadowModel.view = scope.shadowModel.config['fallback-inner-view'];
+    }
+])
