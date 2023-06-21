70f8ea867e0cfb0a2a91da70774b16f606d6e9f2
Lars-Erik <lea@markedspartner.no>
2023-06-22 00:21:25 +0200

    Does static default in BO for (hopefully) all editors

--- src/Umbraco.Community.Fallback/wwwroot/fallback.js
+++ src/Umbraco.Community.Fallback/wwwroot/fallback.js
@@ -1,9 +1,21 @@
 ﻿angular.module('umbraco').controller('umbraco.community.fallback.controller',
     [
         '$scope',
-        function(scope) {
-            scope.shadowModel = JSON.parse(JSON.stringify(scope.model));
-            scope.shadowModel.view = scope.shadowModel.config['fallback-inner-view'];
+        function (scope) {
+            let innerView = scope.model.config['fallback-inner-view'];
+            let modelJson = JSON.stringify(scope.model);
+
+            scope.actualModel = JSON.parse(modelJson);
+            scope.actualModel.view = innerView;
+
+            scope.shadowModel = JSON.parse(modelJson);
+            scope.shadowModel.view = innerView;
+            scope.shadowModel.value = scope.model.config.fallbackTemplate;
+
+            scope.$watch("actualModel.value",
+                function() {
+                    scope.model.value = scope.actualModel.value;
+                }, true);
         }
     ]);
 
@@ -14,6 +26,8 @@ angular.module('umbraco').controller('umbraco.community.fallback.configuration.c
         function (scope, http) {
             scope.dataTypeProperty = scope.preValues.filter(x => x.alias === 'dataType')[0];
             scope.ultimateFallback = {};
+            let initialValue = scope.model.value;
+            let initial = true;
 
             scope.$watch('dataTypeProperty.value',
                 function (newValue) {
@@ -22,6 +36,10 @@ angular.module('umbraco').controller('umbraco.community.fallback.configuration.c
                         .then(response => {
                             console.log(response.data);
                             scope.ultimateFallback = response.data;
+                            if (initial) {
+                                scope.ultimateFallback.value = initialValue;
+                                initial = false;
+                            }
                         });
                 });
 
